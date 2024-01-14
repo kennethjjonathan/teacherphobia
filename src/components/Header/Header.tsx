e24d7886cc753ce07f8940c66f56b42567aae58c
Kenneth Jonathan <kennethjjonathan@gmail.com>
2024-01-14 17:55:48 +0700

    feat: initialized nav bar for heaeder

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,40 @@
+import {
+    Avatar,
+    Link,
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenuToggle,
+} from "@nextui-org/react";
+import TeacherPhobiaLogo from "../TeacherPhobiaLogo/TeacherPhobiaLogo";
+
+const Header = () => {
+  return (
+    <header className="container">
+      <Navbar isBordered maxWidth="full">
+        <NavbarContent justify="start">
+          <NavbarMenuToggle className="sm:hidden" />
+          <NavbarBrand>
+            <TeacherPhobiaLogo />
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent
+          className="hidden gap-4 sm:flex sm:text-small lg:text-medium"
+          justify="center"
+        >
+          <NavbarItem className="cursor-pointer">
+            <Link color="foreground" href="/">
+              Search
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end" className="hidden sm:flex">
+          <Avatar size="sm"/>
+        </NavbarContent>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/TeacherPhobiaLogo/TeacherPhobiaLogo.tsx
@@ -0,0 +1,5 @@
+const TeacherPhobiaLogo = () => {
+  return <p className="text-small font-bold text-foreground lg:text-medium">TeacherPhobia</p>;
+};
+
+export default TeacherPhobiaLogo;
